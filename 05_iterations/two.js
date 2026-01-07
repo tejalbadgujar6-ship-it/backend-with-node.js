c9932eefb2a7129d1fe86ecefb11b8d2044c85f3
tejalbadgujar6-ship-it <tejalbadgujar6@gmail.com>
2026-01-07 11:24:18 +0000

    control flow

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2  
+// }
+
+let myArray = ["flash", "superman", "spiderman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`); // `(back teek) means variables ke sath string bhi print honga
+    arr = arr + 1   
+}
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
