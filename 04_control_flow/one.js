fc4d4de0288dba731dbb90dd17a1dbbade1e9d6c
tejalbadgujar6-ship-it <tejalbadgujar6@gmail.com>
2026-01-07 09:24:39 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,48 @@
+//if
+// const isUserloggedIn = true
+// const temperature = 41
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("tempeature is greater than 50");   
+// }
+// console.log("executed");
+
+//<, >, >=, <=, ==, !=, ===
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log('User power: ${power}');
+// }
+
+// console.log('User power: ${power}');
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less han 750");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// } else {
+//     console.log("less than 1200");
+    
+// }
+
+const UserloggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggdInFromEmail = true
+
+if (UserloggedIn && debitCard && 2==2) {
+    console.log("Allow to buy course");
+} 
+
+if (loggedInFromGoogle || loggdInFromEmail) {
+    console.log("User logged in");
+}
